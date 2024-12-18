dd1a84c790dc03a0f76d1658f1e1a4cf7414eea7
Ananta <codeantu@gmail.com>
2024-12-19 00:14:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const temp = "Hello World";
+
+  return (
+    <>
+     <div>
+        Hello Vite + React!
+     </div>
+    </>
+  )
+}
+
+export default App
