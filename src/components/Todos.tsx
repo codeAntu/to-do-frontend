0acc3923431bebd91b22ac5a69510e530ae428f6
Ananta <codeantu@gmail.com>
2024-12-25 18:07:58 +0530

    Refactor Todo component styling and interaction logic

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -21,19 +21,20 @@ function Todo() {
   const [isSelected, setIsSelected] = React.useState(false)
 
   return (
-      <div
-        className={`flex w-full items-center justify-between rounded-2xl border bg-black/5 p-4 dark:bg-white/5 ${isSelected ? 'border-transparent' : 'border-black/10 dark:border-white/10'} `}
-      >
-        <Checkbox isSelected={isSelected} onValueChange={setIsSelected} lineThrough={isSelected}>
-          <div className=''>Ananta</div>
-        </Checkbox>
-        <div>
-          <Button color='danger' isIconOnly size='sm'
-            variant={`${isSelected ? 'flat' : 'flat'}`}
-          >
-            <TrashIcon />
-          </Button>
-        </div>
+    <div
+      className={`flex w-full items-center justify-between rounded-2xl border-2 p-4 dark:bg-white/5 ${isSelected ? 'bg-black/5' : 'border-2 border-yellow-600/10 bg-yellow-500/10 dark:border-white/10'} `}
+      onClick={() => setIsSelected(!isSelected)} 
+    >
+      <Checkbox isSelected={isSelected} onValueChange={setIsSelected} lineThrough={isSelected}>
+        <div className=''>Ananta</div>
+      </Checkbox>
+      <div>
+        <Button color='danger' isIconOnly size='sm' variant={`${isSelected ? 'light' : 'ghost'}`}
+          className='border '
+        >
+          <TrashIcon size={18} />
+        </Button>
       </div>
-  );
+    </div>
+  )
 }
